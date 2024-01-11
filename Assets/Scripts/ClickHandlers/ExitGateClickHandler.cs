09cb8fe00f0d5236028d86e09b6932eb20dc91dd
GonnaFlyMethod <alex.husakou@gmail.com>
2024-01-11 11:37:05 +0100

    1) Add exit gate click handler files;

---
+++ Assets/Scripts/ClickHandlers/ExitGateClickHandler.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ExitGateClickHandler : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData _)
+    {
+        if (InventorySystem.Instance.IsKeyCollected())
+        {
+            DialogUI.Instance.ShowYesNoDialog("Open", () =>
+            {
+                GameManager.Instance.GameOver();
+            }, () => { });
+
+            return;
+        }
+
+        DialogUI.Instance.ShowMessageOnlyDialog("You need a key!", () => { });
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
